7ace76376641d65215fcca219186cbed5b89dc0e
Matthieu Delmont <matthieu@delmont.fr>
2023-10-15 19:32:59 +0200

    first commit

---
+++ Consignes.md
@@ -0,0 +1,28 @@
+# Ace Attorney Web
+
+Vous êtes un développeur web débutant, et vous avez été engagé pour défendre un client accusé de meurtre. Vous avez reçu les éléments du dossier (assets et page HTML), et vous avez 24h pour préparer votre plaidoirie (votre script JS).
+
+## Consignes
+
+Vous devez réaliser un script JS qui permet de faire parler les différents personnages de l'affaire, de la même façon que le jeu Ace Attorney.
+
+Ce qui comprends :
+- L'affichage du texte lettre par lettre
+- L'affichage des portraits des personnages
+- L'affichage des différents dialogues
+- L'affichage des différentes couleurs de texte (blanc, bleu, rouge et vert, qui sont définies dans les classes CSS `.blue`, `.red` et `.green`)
+- Les effets sonores (musiques, bruitages, machine à écrire lors de l'affichage du texte)
+- Les effets visuels (clignotement des portraits, clignotement du texte, etc.)
+- La possibilité de skip l'affichage lettre par lettre en appuyant sur le bouton de dialogue
+
+Pour tout ça, vous utiliserez :
+- Des objets et des tableaux d'objets
+- Des fonctions, boucles, conditions, etc
+- UN seul événement (click sur le bouton de dialogue)
+- setTimeout et setInterval (respectivement pour l'affichage lettre par lettre et les effets sonores)
+- clearInterval (pour arrêter les setInterval)
+- Audio (pour les musiques et bruitages)
+- ClassList (pour ajouter et retirer des classes CSS)
+- Des expressions régulières (pour détecter les couleurs dans le texte)
+
+Vous êtes libre de travailler comme bon vous semble, on n'est pas dans un module, seulement pour passer un bon moment, ce n'est donc pas évalué DU TOUT !
---
+++ assets/js/script.js
@@ -0,0 +1,163 @@
+let pressedBtp = false;
+let messageIndex = -1;
+let isTyping = false;
+let typeWriterInterval;
+let mute = true;
+// element de l'interface
+const ui = {
+  // Partie messages
+  windowName: document.querySelector("#window_name"),
+  windowMessage: document.querySelector("#window_message"),
+
+  // Partie Personnage
+  character: document.querySelector("#character"),
+  characterBox: document.querySelector("#characterbox"),
+  bg: document.querySelector("#bg"),
+
+  // Nom et message :
+  characterName: document.querySelector("#name"),
+  dialog: document.querySelector("#dialog"),
+
+  // Button UI
+  nextBox: document.querySelector("#nextbox"),
+  nextButton: document.querySelector("#next_button"),
+  arrowButton: document.querySelector("#arrow"),
+};
+
+// élements lié à l'audio
+const audios = {
+  bgm: new Audio("assets/sfx/gumshoe.mp3"),
+  next: new Audio("assets/sfx/bip.wav"),
+  alert: new Audio("assets/sfx/lightbulb.wav"),
+  shake: new Audio("assets/sfx/smack.wav"),
+  male: new Audio("assets/sfx/male.wav"),
+  female: new Audio("assets/sfx/female.wav"),
+};
+
+const scene = [
+  {
+    character: "tektiv",
+    name: "Tektiv",
+    expression: "pumped",
+    message:
+      "Bonjour pour le moment c'est un test je shcerhc un truc pour avoirs unbee fefe!",
+    voice: audios.male,
+  },
+  {
+    character: "tektiv",
+    name: "Tektiv",
+    expression: "mad",
+    message: "C'est le second message pour le moment",
+    voice: audios.male,
+  },
+];
+// change button
+const resetUiButton = () => {
+  ui.nextButton.classList.remove("pressBtn");
+  ui.arrowButton.classList.remove("d-none");
+  ui.nextButton.src = "assets/img/ui/default_button.png";
+  pressedBtp = false;
+};
+
+const pressNextButton = () => {
+  pressedBtp = true;
+  ui.nextButton.classList.add("pressBtn");
+  ui.arrowButton.classList.add("d-none");
+  ui.nextButton.src = "assets/img/ui/pressed_button.png";
+  if (!mute) {
+    audios.next.currentTime = 0;
+    audios.next.volume = 0.4;
+    audios.next.play();
+  }
+};
+const showWindowName = () => {
+  if (ui.windowName.classList.contains("d-none") && scene[messageIndex]?.name) {
+    ui.windowName.classList.remove("d-none");
+  } else if (
+    ui.windowName.classList.contains("d-none") == false &&
+    scene[messageIndex]?.name == null
+  ) {
+    ui.windowName.classList.add("d-none");
+  }
+};
+
+// effect machine à écrire !
+const typeWriter = (currentScene) => {
+  let index = 0;
+  let printedMessage = "";
+  if (typeWriterInterval) {
+    clearInterval(typeWriterInterval);
+  }
+  isTyping = true;
+  setCharacter(currentScene.character, currentScene.expression, "talking");
+  const message = currentScene.message?.trim();
+  typeWriterInterval = setInterval(() => {
+    if (index < message.length) {
+      printedMessage += message[index];
+      index++;
+      ui.dialog.textContent = printedMessage;
+      if (index % 2 == 0 && !mute) {
+        currentScene.voice.currentTime = 0;
+        currentScene.voice.volume = 0.01;
+        currentScene.voice.play();
+      }
+    } else {
+      clearInterval(typeWriterInterval);
+      setCharacter(currentScene.character, currentScene.expression, "silent");
+      isTyping = false;
+    }
+  }, 30);
+};
+
+const showNextDialog = () => {
+  if (messageIndex == 1) {
+    messageIndex = -1;
+
+    ui.windowMessage.classList.add("d-none");
+    ui.windowName.classList.add("d-none");
+    ui.characterName.textContent = null;
+    ui.dialog.textContent = null;
+  } else {
+    messageIndex++;
+    ui.windowMessage.classList.remove("d-none");
+    showWindowName();
+
+    ui.characterName.textContent = scene[messageIndex]?.name;
+
+    typeWriter(scene[messageIndex]);
+  }
+};
+
+// set character sprite
+const setCharacter = (character, expression, mode = "silent") => {
+  ui.character.src = `./assets/img/sprites/${character}/${mode}/${expression}.gif`;
+};
+
+const displayFullMessage = () => {
+  const currentScene = scene[messageIndex];
+  ui.dialog.textContent = currentScene.message;
+  setCharacter(currentScene.character, currentScene.expression, "silent");
+};
+
+// Interaction button
+ui.nextBox.addEventListener("click", () => {
+  if (audios.bgm.paused && !mute) {
+    audios.bgm.loop = true;
+    audios.bgm.volume = 0.05;
+    audios.bgm.play();
+  }
+  if (pressedBtp) return;
+  pressNextButton();
+
+  if (isTyping) {
+    clearInterval(typeWriterInterval);
+    displayFullMessage();
+    resetUiButton();
+    isTyping = false;
+  } else {
+    setTimeout(() => {
+      showNextDialog();
+      resetUiButton();
+    }, 100);
+  }
+});
